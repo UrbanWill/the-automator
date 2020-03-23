1e7079c980e09bf4bfb5374b988be00f935d7c27
will <willian.urban@gmail.com>
2020-03-23 23:24:34 +0900

    initial branch comit

---
+++ tools/history_pull.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from DWZ_ZMQ_Strategy import DWX_ZMQ_Strategy
+
+
+class history_pull():
+    def __init__(self, _name="COIN_FLIP_TRADERS",
+                 _symbols=[('EURCAD')])
+
+                 ## TODO: LEARN PYTHON, FUNCTIONAL OR OOP?
